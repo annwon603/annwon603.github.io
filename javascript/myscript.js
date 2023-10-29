1c6b7207028f6fd18523ff879b5490b774b160f1
annwon603 <101071975+annwon603@users.noreply.github.com>
2023-10-29 00:10:52 -0700

    Update 10.29
    1. Added one more game project
    2. Re-arrange my game project
    3. Starting working on adding tracks

---
+++ javascript/myscript.js
@@ -0,0 +1,7 @@
+// const wavesurfer = WaveSurfer.create({
+//     container: '#waveform',
+//     waveColor: '#4F4A85',
+//     progressColor: '#383351',
+//     url: 'media/Chase Me.mp3',
+//   })
+// wavesurfer.load('media/Chase Me.mp3');
