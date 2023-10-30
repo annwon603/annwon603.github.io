a75998c2ff70203f728848fd9f6e089b319f0a12
annwon603 <101071975+annwon603@users.noreply.github.com>
2023-10-29 19:38:12 -0700

    Move script back to html

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,29 +1,29 @@
-import WaveSurfer from "wavesurfer.js";
+// import WaveSurfer from "wavesurfer.js";
 
-var playBtn = document.getElementById("playBtn");
+// var playBtn = document.getElementById("playBtn");
 
-        const wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: '#BDBFC5',
-        progressColor: '#D9D9D9 ',
-        barWidth: 2 ,
-        responsive: true,
-        height: 30,
-        barRadius: 4, 
-        });
-        wavesurfer.load('../media/Chase Me.mp3');
-        // if player click on button, it will change button
-        playBtn.onclick = function(){
-            wavesurfer.playPause();
-            if(playBtn.getAttribute("src") === "../images/Play Button.png"){
-               playBtn.setAttribute("src", "../images/Pause Button.png");
-            }else{
-               playBtn.setAttribute("src", "../images/Play Button.png");
-            }
-        }
-        //if it finish playing, change the button back to play button
-        //stop the music 
-        wavesurfer.on('finish', function(){
-            playBtn.setAttribute("src", "../images/Play Button.png");
-            wavesurfer.stop();
-        })
+//         const wavesurfer = WaveSurfer.create({
+//         container: '#waveform',
+//         waveColor: '#BDBFC5',
+//         progressColor: '#D9D9D9 ',
+//         barWidth: 2 ,
+//         responsive: true,
+//         height: 30,
+//         barRadius: 4, 
+//         });
+//         wavesurfer.load('../media/Chase Me.mp3');
+//         // if player click on button, it will change button
+//         playBtn.onclick = function(){
+//             wavesurfer.playPause();
+//             if(playBtn.getAttribute("src") === "../images/Play Button.png"){
+//                playBtn.setAttribute("src", "../images/Pause Button.png");
+//             }else{
+//                playBtn.setAttribute("src", "../images/Play Button.png");
+//             }
+//         }
+//         //if it finish playing, change the button back to play button
+//         //stop the music 
+//         wavesurfer.on('finish', function(){
+//             playBtn.setAttribute("src", "../images/Play Button.png");
+//             wavesurfer.stop();
+//         })
