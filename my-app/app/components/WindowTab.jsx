b8827114fcea9bb023f0cb026f753259e5c26f90
annwon603 <aniwong603@gmail.com>
2026-03-04 21:07:43 -0800

    Fixed SpeechBubble
    - About Me Desktop View is finished
    - Will need to start on phone view

--- my-app/app/components/WindowTab.jsx
+++ my-app/app/components/WindowTab.jsx
@@ -3,23 +3,20 @@ import Image from 'next/image';
 
 const RetroWindow = ({ title, width, height, className = '', children }) => {
   return (
-    <div 
-      className={`border border-[#6D6D6D] bg-[#EDEDED] flex flex-col ${className}`}
-      style={{ width, height }}
-    >
+    <div className={`border border-[#6D6D6D] bg-[#EDEDED] flex flex-col ${className}`}
+      style={{ width, height }}>
       {/* Title Bar */}
-      <div className="h-[27px] w-full bg-gradient-to-r from-[#2E2C39] to-[#807B9F] border-b border-[#6D6D6D] flex justify-between items-center px-[8px]">
+      <div className="h-6.75 w-full bg-gradient-to-r from-[#2E2C39] to-[#807B9F] border-b border-[#6D6D6D] flex justify-between items-center px-2">
         {title && (
           <span className="text-white text-[10px] font-bold tracking-tighter uppercase" style={{ fontFamily: "'Dogica Pixel', sans-serif" }}>
             {title}
           </span>
         )}
         {/* Close Button */}
-        <button className="w-[19px] h-[19px] bg-[#D9D9D9] border border-[#6D6D6D] text-[#4F4F4F] text-[11px] font-bold flex items-center justify-center pb-[2px]">
+        <button className="w-4.75 h-4.75 bg-[#D9D9D9] border border-[#6D6D6D] text-[#4F4F4F] text-[11px] font-bold flex items-center justify-center pb-0.5">
           x
         </button>
       </div>
-      
       {/* Window Content */}
       <div className="flex-1 relative">
         {children}
