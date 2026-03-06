062d53cdd4b02361f2cef9e579b791d579251895
annwon603 <aniwong603@gmail.com>
2026-03-05 21:59:53 -0800

    Set up Functionally of Basic Project Section
    - Got Project card and Project section to respond dynamically to different screens
    - Will need to fill out project data
    - Will need to implement in full description pop-up

--- my-app/app/components/ProjectCard.jsx
+++ my-app/app/components/ProjectCard.jsx
@@ -1,30 +1,111 @@
 import React from "react";
+import {LinkIcon, InformationCircleIcon} from "@heroicons/react/24/outline";
+import localFont from "next/font/local";
+import {Rubik,Space_Mono} from 'next/font/google';
+import Link from "next/link";
+
+const rubik = Rubik({
+    subsets: ['latin'],
+    style: ['italic' , 'normal'],
+    preload: true,
+    weight: ['300','400','500','600','700','800','900'],
+
+})
+
+const space_mono = Space_Mono({
+    subsets: ['latin'],
+    style: ['italic' , 'normal'],
+    preload: true,
+    weight: ['400','700'],
+
+})
+
+const dogica = localFont({
+    src: [
+        {
+            path: "../../fonts/dogica.ttf",
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: "../../fonts/dogicabold.ttf",
+            weight: '700',
+            style: 'bold',
+        },
+    ]
+})
+
+const dogicaPixel = localFont({
+    src: [
+        {
+            path: "../../fonts/dogicapixel.ttf",
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: "../../fonts/dogicapixelbold.ttf",
+            weight: '700',
+            style: 'bold',
+        },
+    ]
+})
 
 const codeColors = {
-    "C++" : "bg-["
+    "C++" : "bg-[#FFE8F0]",
+    "Unreal Engine 5" : "bg-[#D2E5EF]",
+    "Unity C#" : "bg-[#E7D2EF]",
+    "FMOD" : "bg-[#D2E5EF]",
+    "WWise" : "bg-[#E3D4D9]"
 }
 
 
-
-const ProjectCard = ({imgURL, title, description, codeStack}) => {
+{/* 1. Mobile First: h-[250px] (Base height for phones) */}
+    {/* 2. Tablet: md:h-[300px] (Standard iPads/Tablets) */}
+    {/* 3. iPad Pro / Laptop: lg:h-[400px] (Portrait iPad Pro) */}
+    {/* 4. Large Desktop: xl:h-[500px] (Landscape iPad Pro / Big Screens) */}
+const ProjectCard = ({imgURL, title, description, codeStack, directURL, fullDescription}) => {
     return(
         <div>
             <div
-                className="h-52 md:h-72" 
-                style={{background: `url(${imgURL})`, backgroundSize: "cover"}}
-                ></div>
-            <div className="text-white">
-                <h5>(title)</h5>
-                <p>(description)</p>
+                className="h-57 lg:h-100 lg:w-180 xl:h-72 xl:w-130 w-full 
+                bg-cover bg-center bg-no-repeat my-4 relative group rounded-md shadow-sm" 
+                style={{backgroundImage: `url(${imgURL})`}}
+            >
+                <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full rounded-md
+                    bg-[#181818] opacity-0 hidden 
+                    group-hover:flex group-hover:opacity-80
+                    transition-duration-500">
+                        <Link 
+                            href={directURL} 
+                            className="h-14 w-14 border-2 mr-6 relative rounded-full border-[#c39fcb] hover:border-white group/link">
+
+                            <LinkIcon className="h-10 w-10 text-[#c39fcb] absolute top-1/2 left-1/2 transform -translate-x-1/2 
+                            -translate-y-1/2 cursor-pointer group-hover/link:text-white ">
+                            </LinkIcon>
+                            
+                        </Link>
+                        <Link href="/" className="h-14 w-14 border-2 relative rounded-full border-[#c39fcb] hover:border-white group/link">
+                            <InformationCircleIcon className="h-10 w-10 text-[#c39fcb] absolute top-1/2 left-1/2 transform -translate-x-1/2 
+                            -translate-y-1/2 cursor-pointer group-hover/link:text-white ">
+                            </InformationCircleIcon>
+                        </Link>
+                </div>
+            </div>
+
+            <div className="text-[#2E2C39] mt-1">
+                <h5 className={`${dogicaPixel.className}`}>{title}</h5>
+                <p className={`${rubik.className} my-2`}>{description}</p>
             </div>
             {/*Button */}
-            <div className="flex-row gap-2 mt-4">
-                {codeStack.map((code,index) => (
-                    <span key={index} className="bg-amber-50 border-2 rounded-2xl border-black">
+            <div className="flex flex-row flex-wrap gap-2 mt-4">
+                {codeStack.map((code,index) => {
+                    const dynamicClass = codeColors[code] || "bg-[#EDEDED] text-[#2E2C39]";
+                    return(
+                    <span key={index} className={`${space_mono.className} border-2 rounded-lg p-1 px-2 border-[#6D6D6D] text-[#6D6D6D] ${dynamicClass}`}>
                         {code}
                     </span>
-                ))}
-
+                );
+                })}
             </div>   
         </div>
     )
--- my-app/app/components/ProjectSection.jsx
+++ my-app/app/components/ProjectSection.jsx
@@ -32,34 +32,71 @@ const dogicaPixel = localFont({
     ]
 })
 
-const ProjectData = [
-    {
+const projectData = [
+    {    
         id: 1,
         title: "Mariposa",
-        description: "N/A",
-        image: "",
+        description: "Audio Lead, Sound Designer, and Composer for 2D storytelling puzzle platformer game now available on Steam.",
+        image: "/images/project_thumbnail/Mariposa.jpg",
+        directLink: "https://store.steampowered.com/app/3991120/Mariposa/",
+        full_description: "",
+        tag: ["Unity C#", "FMOD"],
         
     },
     {
-
+        id: 2,
+        title: "Out of Tune",
+        description: "N/A",
+        image: "/images/project_thumbnail/Mariposa.jpg",
+        directLink: "/",
+        full_description: "blah",
+        tag: [],
     },
     {
-
+        id: 3,
+        title: "Heartwood",
+        description: "N/A",
+        image: "",
+        directLink: "/",
+        full_description: "blah",
+        tag: [],
     },
     {
-
+        id: 4,
+        title: "Choros",
+        description: "N/A",
+        image: "",
+        directLink: "/",
+        full_description: "blah",
+        tag: [],
     },
     {
-
+        id: 5,
+        title: "Serious Games: Climate Change",
+        description: "N/A",
+        image: "",
+        directLink: "/",
+        full_description: "blah",
+        tag: [],
     }
 ]
 
 const ProjectSection = () => {
     return(
-        <div className="mx-24 my-20">
+        <div className="mx-4 sm:mx-10 md:mx-40 my-20">
             <h2 className={`${dogicaPixel.className} text-[#2E2C39] font-bold text-lg`}>
                 Projects 
             </h2>
+            <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 md:gap-12 mt-6">
+                {projectData.map((project) => 
+                <ProjectCard 
+                key={project.id} 
+                title={project.title} 
+                description={project.description} 
+                imgURL={project.image}
+                directURL={project.directLink}
+                fullDescription={project.full_description}
+                codeStack={project.tag} />)}</div>
         </div>
         
     )
